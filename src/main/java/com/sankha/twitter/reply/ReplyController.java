61c4b536f3ff853a9c2844f5ae5098964e32eef6
Sankhadeep Chatterjee <sankhadeep2007@gmail.com>
2023-03-28 18:50:01 +0530

    Initial commit

---
+++ src/main/java/com/sankha/twitter/reply/ReplyController.java
@@ -0,0 +1,47 @@
+package com.sankha.twitter.reply;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.*;
+
+import com.sankha.twitter.response.ApiResponse;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/tweets")
+public class ReplyController {
+	@Autowired
+	ReplyService replyService;
+	@Autowired
+	ApiResponse apiResponse;
+	
+	@PostMapping(path = "/tweet/{tweet_id}/reply", produces = "application/json")
+	public ResponseEntity<Object> userMakesCommentAtTweet(
+			@RequestBody Reply reply, 
+			Authentication authentication,
+			@PathVariable("tweet_id") long tweet_id 
+	) throws Exception{
+		Reply savedComment = replyService.userMakesNewCommentAtTweet(authentication, tweet_id, reply);		
+		apiResponse.setData(savedComment);
+		apiResponse.setMessage("Comment crated");
+		return new ResponseEntity<>(apiResponse.getBodyResponse(),HttpStatus.OK);
+	}
+
+
+	@GetMapping(path = "/tweet/{tweet_id}/reply", produces = "application/json")
+		  public ResponseEntity<Object> getUsersCommentAtTweet(
+			@RequestBody Reply reply,
+						Authentication authentication,
+			@PathVariable("tweet_id") long tweet_id
+				  ) throws Exception{
+		   List<Reply> savedComments = replyService.showTweetComments(tweet_id);
+		   System.out.println(savedComments);
+		   apiResponse.setMessage("Comment crated");
+		   apiResponse.setData(savedComments);
+		   return new ResponseEntity<>(apiResponse.getBodyResponse(),HttpStatus.OK);
+		  }
+
+}
---
+++ src/main/java/com/sankha/twitter/tweet/Tweet.java
@@ -0,0 +1,52 @@
+package com.sankha.twitter.tweet;
+
+import java.sql.Timestamp;
+import java.util.List;
+
+import javax.persistence.*;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.sankha.twitter.like.LikeEntity;
+import com.sankha.twitter.reply.Reply;
+import com.sankha.twitter.user.UserEntity;
+import com.sankha.twitter.media.Media;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Tweet {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long tweetId;
+	
+	private String text;
+	private Timestamp created;
+	private Timestamp updated;
+	
+	@ManyToOne
+	private UserEntity tweetAuthor;
+	
+	@OneToMany(fetch = FetchType.LAZY,mappedBy = "tweet",cascade = CascadeType.ALL)
+	//@JoinColumn(name="replies")
+	private List<Reply> replies;
+	
+	
+	@OneToMany(fetch = FetchType.LAZY)
+	@JoinColumn(name="likes")
+	private List<LikeEntity> likes;
+	
+	
+	@OneToMany(fetch = FetchType.LAZY)
+	@JoinColumn(name="media")
+	private List<Media> medias;
+	
+	
+	
+	
+	
+}
